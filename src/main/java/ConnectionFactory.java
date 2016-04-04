f844d3d91eb5f0bb205d8223d5af4db9b18ad71d
Bogdan <razdpl@gmail.com>
2016-04-04 16:33:09 +0300

    second commit

--- src/main/java/ConnectionFactory.java
+++ src/main/java/ConnectionFactory.java
@@ -12,6 +12,8 @@
         private static final String dbUser = "root";
         private static final String dbPassword = "root";
 
+
+
         private static String dbUrl = "jdbc:mysql://" + dbHost + "/" + dbName;
 
         public static Connection getConnection() throws SQLException {
